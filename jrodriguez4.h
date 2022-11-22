61558eab12bff664d78e8485bb56ebff6dd1d9df
Jason Rodriguez <111898010+rodriguezjason807@users.noreply.github.com>
2022-11-22 14:57:08 -0800

    Manually Entered queue code

--- jrodriguez4.h
+++ jrodriguez4.h
@@ -1,7 +1,40 @@
 // Jason's Include file 
-// Update Date : October 4 2022
+// Update Date : November 2  2022
+#include <iostream>
+#include <string>
 
-extern void game_log(int xres, int yres); 
-extern void feature_border(int xres, int yres);
-extern unsigned int toggle(unsigned int toggle);
-extern void show_help(int xres, int yres);
+using namespace std;
+
+//
+//QUEUE FOR STRINGS ( size of 5 )
+//
+
+class Queue {
+
+	private:
+		int front, rear;
+	public:
+		string arr[5];
+	Queue() {
+		front = -1;
+		rear = -1;
+		for ( int i=0; i<5; i++) {
+			arr[i] = " ---- ";
+		}
+	}
+	bool isEmpty();
+	bool isFull();
+	void enqueue(string action);
+	void dequeue();
+	int size();
+	void showQueue();
+};
+
+//
+//FUNCTIONS
+//
+void log_print(Queue queueName, int xres, int yres); // shows log window 
+void log_window(int xres, int yres); // shows log window 
+void feature_border(int xres, int yres); // enables feature mode
+void show_help(int xres, int yres); // shows help window
+unsigned int toggle(unsigned int toggle); // used to toggle variables on/off
