3c496f055c5b7ada6070b5bb0ce2ba8c592337ae
tayhooser <84413327+tayhooser@users.noreply.github.com>
2022-11-22 13:29:34 -0800

    revert jasons changes
    jasons changes edited some weirdo stuff

--- jrodriguez4.cpp
+++ jrodriguez4.cpp
@@ -1,106 +1,38 @@
-// Purpose : Personal code file
-// Author : Jason R.
-// Update Date : November 22  2022
 
-#include <iostream> // <--- output/input functions
-#include <string>	// <--- string functions
-#include <stdio.h>	// <--- output/input functions 
-#include <stdlib.h>	
-#include <GL/glx.h>	// <--- OpenGl functions and utilities
-#include "fonts.h"	
-#include "jrodriguez4.h"
+// Individual source file
+// Author: Jason Rodriguez
+// Update Date : October 4 2022
+#include <stdio.h>
+#include <GL/glx.h>
+#include "fonts.h"
 
+int  show_jason() {
 
-//|=========================|
-//| Check if queue is empty |
-//|=========================|
-bool Queue::isEmpty() {
-	
-	if ( front == -1 && rear == -1 )
-		return true;
-	else
-		return false;
+	printf("Jason\n");
+	return 0;
 }
 
-//|========================|
-//| Check if queue is full |
-//|========================|
-bool Queue::isFull() {
-
-	if (rear == 4 )
-		return true;
-	else
-		return false;
-}
-
-//|==========================|
-//| Insert string into queue |
-//|==========================|
-void Queue::enqueue(string action) {
-	if ( isFull() ) {
-		// cout << " FULL " << endl
-		dequeue();
-		enqueue(action);
-	} else if ( rear == -1 && front == -1 ) {
-		rear = front = 0;
-		arr[rear] = action;
-	} else {
-		rear++;
-		arr[rear] = action;
-	}
-}
-
-
-//|==========================|
-//| Delete string from queue |
-//|==========================|
-void Queue::dequeue() {
-	string temp = " ---- ";
-	if ( isEmpty() ) {
-		cout << "Can't dequeue an empty queue dummy" << endl;
-	} else if ( isFull() ) {
-		for ( int i=0; i <= 3; i++) {
-			arr[i] = arr[i+1];
-		}
-		arr[rear] = temp;
-		rear--;
-	} else {
-		arr[rear] = temp;
-		rear--;
-	}
-}
-
-//|=====================|
-//| Check size of queue |
-//|=====================|
-int Queue::size() {
-
-	return (rear - front + 1);
-}
-
-//|==============================|
-//| Shows values inside of queue |
-//|==============================|
-void Queue::showQueue() {
-
-	cout << "All values in the queue are - " << endl;
-	for ( int i=0; i<5; i++) {
-		cout << arr[i] << " \n";
-	}
-}
-
-//|=============================================|
-//| allows for toggling of int variables on/off |
-//|=============================================|
 unsigned int toggle(unsigned int toggle) {
 
 	toggle = toggle ^ 1;
 	return toggle;
 }
 
-//|===================================|
-//| Border to show feature mode is on |
-//|===================================|
+void game_log(int xres, int yres) {
+
+	int xcent = xres / 2;		
+	int ycent = yres / 2;		
+	int w = 90;
+	int h = 190;
+    glColor3f(0.6, 0.6, 0.6);
+	glBegin(GL_QUADS);
+		glVertex2f(xcent-w, ycent-h);
+        glVertex2f(xcent-w, ycent+h);
+        glVertex2f(xcent+w, ycent+h);
+        glVertex2f(xcent+w, ycent-h);
+    glEnd();
+}
+
 void feature_border(int xres, int yres) {
 
 	// Triangle strip border
@@ -130,9 +62,6 @@ void feature_border(int xres, int yres) {
 
 }
 
-//|==============|
-//| Enables help |
-//|==============|
 void show_help( int xres, int yres) {
 
     Rect r;
@@ -151,13 +80,13 @@ void show_help( int xres, int yres) {
     r.left = xcent;
     r.bot = ycent + 140;
     r.center = 50;
-
+        
     ggprint16(&r, 50, 0xffffffff, " --- HELP ---");
     ggprint16(&r, 25, 0xffffffff, " cycle 'F2' or press 'F' for Danny's feature");
     ggprint16(&r, 25, 0xffffffff, " press ??? for Cecilio's feature");
-    ggprint16(&r, 25, 0xffffffff, " press 'd' for Delaney's feature");
+    ggprint16(&r, 25, 0xffffffff, " press ??? for Delaney's feature");
     ggprint16(&r, 25, 0xffffffff, " cycle 'F2' for Taylor's feature");
-    ggprint16(&r, 25, 0xffffffff, " press 'a' for Jason's feature (test with x and z)");
+    ggprint16(&r, 25, 0xffffffff, " press ??? for Jason's feature");
     
     ggprint16(&r, 25, 0xffffffff, "");
     
@@ -167,63 +96,6 @@ void show_help( int xres, int yres) {
     ggprint16(&r, 25, 0xffffffff, " press 'F1' for Help");
     ggprint16(&r, 25, 0xffffffff, " press 'i' for Intro");
     ggprint16(&r, 25, 0xffffffff, " press 'Esc' to quit game");
-
-}
-
-//|====================|
-//| Enables log window |
-//|====================|
-void log_window(int xres, int yres) {
-
-    Rect r;
-    int xcent = xres / 2;
-    int ycent = yres / 2;
-    int w = xres/12.5;
-    int h = yres/3;
-	glEnable(GL_BLEND);
-    glColor4f(0.6, 0.6, 0.6, 0.7);
-    glBegin(GL_QUADS);
-        glVertex2f(xcent-w, ycent-h);
-        glVertex2f(xcent-w, ycent+h);
-        glVertex2f(xcent+w, ycent+h);
-        glVertex2f(xcent+w, ycent-h);
-    glEnd();
-    
-    r.left = xcent;
-    r.bot = ycent+(h - 50);
-    r.center = 50;
-
-	ggprint16(&r, 50, 0xfffffff0, " THEE LOG ");
-}
-
-//|================|
-//| Print log text |
-//|================|
-void log_print(Queue queueName, int xres, int yres) {
-
-Rect r;
-    int xcent = xres / 2;
-    int ycent = yres / 2;
-    //int w = xres/12.5;
-    int h = yres/3;
-	
-    r.left = xcent;
-    r.bot = ycent+(h - 100);
-    r.center = 50;
-	
-	
-	ggprint16(&r, 25, 0xfffffff0, "  ");
-	ggprint16(&r, 25, 0xfffffff0, queueName.arr[0].c_str());
-	ggprint16(&r, 25, 0xfffffff0, "  ");
-	ggprint16(&r, 25, 0xfffffff0, queueName.arr[1].c_str());
-	ggprint16(&r, 25, 0xfffffff0, "  ");
-	ggprint16(&r, 25, 0xfffffff0, queueName.arr[2].c_str());
-	ggprint16(&r, 25, 0xfffffff0, "  ");
-	ggprint16(&r, 25, 0xfffffff0, queueName.arr[3].c_str());
-	ggprint16(&r, 25, 0xfffffff0, "  ");
-	ggprint16(&r, 25, 0xfffffff0, queueName.arr[4].c_str());
-
-
 
 }
 
